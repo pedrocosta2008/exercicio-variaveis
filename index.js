bbc493d9dd8617bbc1c5fd78bbba594da8c29115
pedro H costa <137086984+pedrocosta2008@users.noreply.github.com>
2023-08-22 21:47:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+//let variaveltexto = "texto";
+//let variavelnumero = 6;
+//console.log(typeof variaveltexto)
+//console.log(typeof variavelnumero)
+
+//variavel vazia
+//let variavelvazia;
+//console.log(typeof variavelvazia);
+
+//let variavelNula = null;
+//console.log(typeof variavelNula);
+
+//let a = "b";
+//console.log(a);
+//console.log(typeof a);
+
+//exercicio 2 
+
+let nome;
+let idade;
+let numero = 6;
+
+nome = prompt("qual seu nome?");
+idade = prompt("qual a sua idade?");
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof numero + 5);
+console.log(numero.toString() +5);
+
+//conversao numero
+
+let N = "42";
+console.log(Number(N));
+console.log(typeof N);
+console.log(typeof Number(N));
+
+//Exercicios---
