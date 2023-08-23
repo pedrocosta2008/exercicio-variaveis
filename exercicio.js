c0a0278001f6123599cefea2cedaf6c76ad34f91
pedro H costa <137086984+pedrocosta2008@users.noreply.github.com>
2023-08-22 21:45:55 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,35 @@
+/*
+*Pedro Costa
+*
+*/
+
+//Exercicios de interpretação
+
+// 1. A=10 B=5
+
+// 2. A=10 B=10 C=10
+
+//3. Um nome melhor para Variavel(p) é "Horas de trabalho por dia"
+//Um nome melhor para Variavel(t) é "Valor diario "
+
+//Exercicios de escrita
+
+// 1.
+
+let nome;
+let idade;
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+//foi escrito UNDEFINED pois não atribuimos um valor
+
+nome = prompt("qual seu nome?");
+idade = prompt("qual a sua idade?");
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+//Anotei a palavra "object"
+
+console.log("ola", nome, "voce tem" idade)
